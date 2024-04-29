94325a43a2ceb401b76a4161f817340c9e295464
Yahya Mohamed <yahyamohamed1231@outlook.com>
2024-04-29 18:02:09 +0100

    push solution to q9

--- q9/src/index.js
+++ q9/src/index.js
@@ -4,7 +4,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({ message: "docker is easy" });
 });
-const port = 8080;
+const port = 3000;
 app.listen(port, () => {
   console.log(`app is listening at port ${port}`);
 });
